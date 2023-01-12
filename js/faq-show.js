914e3917f7763b84b40453d016b1a99b28245d90
Jiu Yang <g-jiuyang@gashpoint.com>
2023-01-12 14:34:07 +0800

    更新FAQ、新增Partners & footer

--- js/faq-show.js
+++ js/faq-show.js
@@ -1,11 +1,17 @@
 $('#faq dt').click(function () {
 	$(this).siblings().removeClass('show');
 	$(this).toggleClass('show');
+
+	var nowOffset = $(this).offset().top;
+	var dlHeight = $(document).innerHeight() - $(document).innerWidth() * 0.06 - 128;
+	if (nowOffset < dlHeight / 2) {
+		$(this).parent().animate({
+			scrollTop: 0
+		}, 'slow');
+	}
+	if (nowOffset >= dlHeight / 2) {
+		$(this).parent().animate({
+			scrollTop: $(this).parent().scrollTop() + 100
+		}, 'slow');
+	}
 })
-
-var currentY = 0;
-
-$(window).scroll(function () {
-	currentY = $('#faq').offset().top;
-	console.log(currentY);
-});
